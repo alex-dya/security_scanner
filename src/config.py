5617214b096a635932e6745e5e604a6226c7b9d3
wolf <Tiunov.li.174@gmail.com>
2018-08-04 09:21:57 +0300

    Added database

--- src/config.py
+++ src/config.py
@@ -1,5 +1,15 @@
-TITLE = 'Security Scanner'
+import os
+from pathlib import Path
 
-CSRF_ENABLED = True
-SECRET_KEY = 'CHANGE_ME'
-DEBUG = True
+
+class Config:
+    TITLE = 'Security Scanner'
+    CSRF_ENABLED = True
+    SECRET_KEY = 'CHANGE_ME'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        f'sqlite:///{Path(Path.cwd(), "app.db")}'
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from web import app
+from web import app, db
+from web.models import User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
