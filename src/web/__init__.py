987e5197c307f3da02aac3e9e2effe3759444c8a
wolf <Tiunov.li.174@gmail.com>
2018-08-05 17:23:18 +0300

    Added flask_bootstrap

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -11,6 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+bootstrap = Bootstrap(app)
 
 
 from web import views, models, credentials, profiles
