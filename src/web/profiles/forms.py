f53a9e3e85bdc66c41164ac9c913aaac1344bf29
wolf <Tiunov.li.174@gmail.com>
2018-09-01 18:54:20 +0300

    Added postgres to edit profile form.

--- src/web/profiles/forms.py
+++ src/web/profiles/forms.py
@@ -52,6 +52,31 @@ class UnixSettings(FlaskForm):
         self.root_password.data = settings['unix'].get('root_password')
 
 
+class PostgresSettings(FlaskForm):
+    port = IntegerField(
+        _l('Port'),
+        validators=[DataRequired(), NumberRange(min=0, max=65535)],
+        default=5432
+    )
+    dbname = StringField(
+        _l('Database name'),
+        validators=[DataRequired()]
+    )
+    credential = QuerySelectField(
+        _l('Credentials'),
+        get_label='name',
+        query_factory=get_credentials
+    )
+
+    def populate(self, settings: Dict[str, Dict[str, str]]) -> None:
+        postgres_setting = settings['postgres']
+        self.port.data = int(postgres_setting.get('port') or 22)
+        self.dbname.data = postgres_setting.get('dbname')
+        cred = postgres_setting.get('credential')
+        if cred:
+            self.credential.data = AccountCredential.query.get(int(cred))
+
+
 class ScanProfileForm(FlaskForm):
     id = HiddenField(_l('Id'))
     name = StringField(
@@ -62,6 +87,7 @@ class ScanProfileForm(FlaskForm):
         ])
     ssh_settings = FormField(SSHSettings)
     unix_settings = FormField(UnixSettings)
+    postgres_settings = FormField(PostgresSettings)
     submit = SubmitField(_l('Save'))
 
     def populate(self, profile: ScanProfile = None) -> None:
@@ -74,6 +100,7 @@ class ScanProfileForm(FlaskForm):
 
         self.ssh_settings.populate(settings)
         self.unix_settings.populate(settings)
+        self.postgres_settings.populate(settings)
 
     def populate_obj(self, profile: ScanProfile) -> None:
         profile.name = self.name.data
@@ -88,6 +115,9 @@ class ScanProfileForm(FlaskForm):
             ('unix', 'privilege_escalation',
              self.unix_settings.privilege_escalation),
             ('unix', 'root_password', self.unix_settings.root_password),
+            ('postgres', 'credential', self.postgres_settings.credential),
+            ('postgres', 'port', self.postgres_settings.port),
+            ('postgres', 'dbname', self.postgres_settings.dbname),
         ]
 
         for transport, option, field in all_settings:
@@ -105,4 +135,3 @@ class ScanProfileForm(FlaskForm):
             else:
                 value = str(field.data)
             setting.value = value
-
